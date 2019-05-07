c1b418c0e8d069627807f0a2019c9e986fa39952
patpalmerston <patpalmerston@gmail.com>
2019-05-07 12:15:58 -0700

    CRUD functioning

--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,28 +1,94 @@
 const router = require('express').Router();
+const knex = require('knex');
+
+// this configuration object teaches knex hot to find the database and what driver to use.
+const knexConfig = {
+  client: 'sqlite3', // the npm/yarn module we installed
+  useNullAsDefault: true, // needed when working with SQLite3
+  connection: {
+    // relative to the file folder
+    filename: './data/rolex.db3'
+  }
+}
+
+const db =knex(knexConfig);
 
 router.get('/', (req, res) => {
   // get the roles from the database
-  res.send('Write code to retrieve all roles');
+  db('roles')
+    .then(roles => {
+      res.status(200).json(roles)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
 });
 
+// remember to get rid of the array brackets by adding first()
 router.get('/:id', (req, res) => {
   // retrieve a role by id
-  res.send('Write code to retrieve a role by id');
+  db('roles')
+    .where({id: req.params.id})
+    .first() // or you can reference array at first location, first() has same effect
+    .then(role => {
+      if(role) {
+        res.status(200).json(role)
+      } else {
+        res.status(404).json({ message: 'Rold Id not found' })
+      }
+    }).catch(err => {
+      res.status(500).json(err)
+    })
 });
 
 router.post('/', (req, res) => {
   // add a role to the database
-  res.send('Write code to add a role');
+   db('roles')
+    .insert(req.body)
+    .then(role => {
+      const [id] = role;
+
+      db('roles')
+        .where({ id })
+        .first()
+        .then(role => {
+          res.status(200).json(role)
+        })
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update roles
-  res.send('Write code to modify a role');
+ const changes = req.body;
+ const { id } = req.params;
+
+ db('roles')
+  .where({ id: id })
+  .update(changes)
+  .then(count => {
+    res.status(200).json(count)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // remove roles (inactivate the role)
-  res.send('Write code to remove a role');
+  const { id } = req.params;
+
+  db('roles')
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count)
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;
